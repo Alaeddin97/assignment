315dc95a2b1bdf2de601a67fcea567ce4976b63b
Ala <ala>
2022-06-21 05:34:15 +0200

    first commit

--- src/main/java/com/assignment/springboot/repository/AssignmentRepository.java
+++ src/main/java/com/assignment/springboot/repository/AssignmentRepository.java
@@ -1,2 +1,13 @@
-package com.assignment.springboot.repository;public interface AssignmentRepository {
+package com.assignment.springboot.repository;
+
+import com.assignment.springboot.domain.Assignment;
+import com.assignment.springboot.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface AssignmentRepository extends JpaRepository<Assignment,Long> {
+    Set<Assignment> findByUser(User user);
 }
--- src/main/java/com/assignment/springboot/web/AssignmentController.java
+++ src/main/java/com/assignment/springboot/web/AssignmentController.java
@@ -1,2 +1,35 @@
-package com.assignment.springboot.web;public class AssignmentController {
+package com.assignment.springboot.web;
+
+import com.assignment.springboot.domain.Assignment;
+import com.assignment.springboot.domain.User;
+import com.assignment.springboot.service.AssignmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.SecondaryTable;
+import java.util.Optional;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/assignments")
+public class AssignmentController {
+    @Autowired
+    private AssignmentService assignmentService;
+    @PostMapping("")
+    public ResponseEntity<?> createAssignment(@AuthenticationPrincipal User user ){
+        Assignment newAssignment=assignmentService.save(user);
+        return ResponseEntity.ok(newAssignment);
+    }
+    @GetMapping("")
+    public ResponseEntity<?> getAssignments(@AuthenticationPrincipal User user){
+        Set<Assignment>assignmentSet=assignmentService.assignmentSet(user);
+        return ResponseEntity.ok(assignmentSet);
+    }
+    @GetMapping("{assignmentID}")
+    public ResponseEntity<?> getOneAssignment(@PathVariable Long assignmentID, @AuthenticationPrincipal User user){
+        Optional<Assignment>assignmentOpt=assignmentService.findById(assignmentID);
+        return ResponseEntity.ok(assignmentOpt.orElse(new Assignment()));
+    }
 }
